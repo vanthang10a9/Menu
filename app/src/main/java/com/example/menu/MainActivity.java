d16e67ada52a1f97ded5e3d93a602dea976b403c
vanthang10a9 <levanthang510@gmail.com>
2019-08-30 19:50:26 +0700

    thêm activity, Random, - lỗi show alertdialog

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText mEditText;
+    private TextView mTextView;
+    private Button mButton;
+    private Integer x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.edittext);
+        mTextView = (TextView) findViewById(R.id.textview);
+        mButton = (Button) findViewById(R.id.button);
+
+        System.out.println(x);
+
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Random random = new Random();
+//                x = random.nextInt(5 + 2);
+//                mTextView.setText(x.toString());
+                Intent intent=new Intent(MainActivity.this,Random.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
